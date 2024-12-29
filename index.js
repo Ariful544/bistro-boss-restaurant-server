5d146241e19713c22918654973476e1978f7573f
MD. Ariful islam <khidirerbari@gmail.com>
2024-12-30 02:18:26 +0300

    menu and reviews api done

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 3000;
+
+app.use(express());
+app.use(cors());
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vedvc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const menuCollections = client.db("bistroBossRestaurant").collection("menu");
+    const reviewCollections= client.db("bistroBossRestaurant").collection("reviews");
+
+    app.get("/", async (req, res) => {
+      res.send("Welcome to bistro boss restaurant!");
+    });
+    app.get("/menu", async (req, res) => {
+        const result = await menuCollections.find().toArray();
+        res.send(result);
+    });
+    app.get('/reviews', async (req, res) => {
+        const result = await reviewCollections.find().toArray();
+        res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
